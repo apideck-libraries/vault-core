2079e0f4ff21a308d470bbe39821b1dbf4af70e1
Jake Prins <jakeprins@outlook.com>
2022-01-28 15:42:57 +0100

    Implement modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,87 @@
+import { Dialog, Transition } from '@headlessui/react';
+import React, { CSSProperties, Fragment, useEffect, useState } from 'react';
+
+import Logo from './Logo';
+import { createPortal } from 'react-dom';
+
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  onClose: () => void;
+  isOpen: boolean;
+  className?: string;
+  style?: CSSProperties;
+  showAttribution?: boolean;
+}
+
+export function Modal({
+  children,
+  onClose,
+  isOpen,
+  showAttribution,
+  className = '',
+  style = {},
+}) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const modalComponent = (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto bg-gray-400 bg-opacity-75"
+        onClose={onClose}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full text-left max-w-md p-6 my-8 align-middle transition-all transform bg-white shadow-xl rounded-xl">
+              {children}
+            </div>
+          </Transition.Child>
+          {showAttribution ? (
+            <a
+              className="absolute flex text-sm text-center text-gray-100 bottom-5 left-[calc(50% - 85px)] lg:left-5 xl:left-6 xl:bottom-6"
+              href="https://apideck.com/products/unify"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by <Logo className="h-5" />
+            </a>
+          ) : null}
+        </div>
+      </Dialog>
+    </Transition>
+  );
+
+  return mounted ? createPortal(modalComponent, document.body) : null;
+}
+
+export default Modal;
