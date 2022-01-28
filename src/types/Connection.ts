5993e3dc9aec054ee8600add015ade521006368b
Jake Prins <jakeprins@outlook.com>
2022-01-28 15:45:14 +0100

    Add connection interface

---
+++ src/types/Connection.ts
@@ -0,0 +1,46 @@
+import { FormField } from './FormField';
+import { RawJSON } from './RawJson';
+
+export interface Settings extends RawJSON {
+  instance_url?: string;
+  base_url?: string;
+}
+
+export type ConnectionState = 'available' | 'added' | 'authorized' | 'callable';
+
+export interface Connection {
+  id: string;
+  service_id: string;
+  unified_api: string;
+  auth_type: string | null;
+  name: string;
+  icon: string;
+  logo?: string;
+  website?: string;
+  tag_line?: string;
+  authorize_url?: string;
+  revoke_url?: string | null;
+  state: ConnectionState;
+  enabled?: boolean;
+  settings?: Settings;
+  settings_required_for_authorization?: string[];
+  configurable_resources: string[];
+  resource_schema_support: string[];
+  configuration?: FormField[];
+  form_fields: FormField[];
+  created_at: number;
+}
+
+export interface UpdateConnectionInput {
+  enabled: boolean;
+  settings?: Settings;
+}
+
+export interface UpdateConnectionConfigInput {
+  configuration: ResourceConfig[];
+}
+
+export interface ResourceConfig {
+  resource: string;
+  defaults: Partial<FormField>[];
+}
