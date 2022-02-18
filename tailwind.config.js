1a5564ea3a645935ac67bdca147dd3cb0ad4cf3d
Jake Prins <jakeprins@outlook.com>
2022-02-18 08:34:42 +0100

    Test not wrapping componet in custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
-const config = require('@apideck/components/tailwind-config');
+// const config = require('@apideck/components/tailwind-config');
 
-module.exports = config({
+// module.exports = config({
+//   content: [
+//     './src/**/*.{html,js,jsx,ts,tsx}',
+//     './node_modules/@apideck/components/dist/*.js',
+//     './stories/*',
+//   ],
+// });
+
+module.exports = {
   content: [
     './src/**/*.{html,js,jsx,ts,tsx}',
-    './node_modules/@apideck/components/dist/*.js',
+    './node_modules/@apideck/components/**/*.js',
     './stories/*',
   ],
-});
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f6f7fe',
+          100: '#f2f3fd',
+          200: '#e0e1fa',
+          300: '#c9c8f4',
+          400: '#aba4ea',
+          500: '#9182de',
+          600: '#775ad8',
+          700: '#6434d5',
+          800: '#5922b9',
+          900: '#5a1aa8',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
