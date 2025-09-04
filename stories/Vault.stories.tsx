c43941f5da1287698d1f7b0c68cf4f1e45000e4e
Geiser Menoia <gmenoiaa@gmail.com>
2025-09-04 10:09:47 -0300

    Refactor AuthorizeButton to auto-start authorization; add new story for auto-start feature

--- stories/Vault.stories.tsx
+++ stories/Vault.stories.tsx
@@ -30,7 +30,7 @@ const Template: Story<Props> = (args) => (
     {...args}
     showConsumer={true}
     // unifyBaseUrl="http://localhost:3050"
-    token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aGVtZSI6eyJsb2dvIjoiaHR0cHM6Ly93d3cuYXBpZGVjay5jb20vZmF2aWNvbi5pY28ifSwiY29uc3VtZXJfaWQiOiJkZW1vLXJlYWN0LXZhdWx0LWc1M2YwYS0yMDI1LTA3LTAxVDE0OjE0OjI3LjkxOVoiLCJhcHBsaWNhdGlvbl9pZCI6IkFpQ2t6eGNwWld1eFVSTVVrYTlmOVdPVVJoa09iMEh4cEtVa2EiLCJzY29wZXMiOltdLCJpYXQiOjE3NTE0NDQ5MjIsImV4cCI6MTc1MTQ0ODUyMn0.FSW2oZ1WKy2JjcrAMEnhkXp15nLDd73pJYnGYTpCmns"
+    token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWRpcmVjdF91cmkiOiI8c3RyaW5nPiIsImNvbnN1bWVyX21ldGFkYXRhIjp7ImFjY291bnRfbmFtZSI6IjxzdHJpbmc-IiwidXNlcl9uYW1lIjoiPHN0cmluZz4iLCJlbWFpbCI6IjxzdHJpbmc-IiwiaW1hZ2UiOiI8c3RyaW5nPiJ9LCJjdXN0b21fY29uc3VtZXJfc2V0dGluZ3MiOnsiZWl1c21vZF8xMSI6NDkwMjI5NiwidmVuaWFtX2MiOmZhbHNlfSwidGhlbWUiOnsiZmF2aWNvbiI6IjxzdHJpbmc-IiwibG9nbyI6IjxzdHJpbmc-IiwicHJpbWFyeV9jb2xvciI6IjxzdHJpbmc-Iiwic2lkZXBhbmVsX2JhY2tncm91bmRfY29sb3IiOiI8c3RyaW5nPiIsInNpZGVwYW5lbF90ZXh0X2NvbG9yIjoiPHN0cmluZz4iLCJ2YXVsdF9uYW1lIjoiPHN0cmluZz4iLCJwcml2YWN5X3VybCI6IjxzdHJpbmc-IiwidGVybXNfdXJsIjoiPHN0cmluZz4ifSwic2V0dGluZ3MiOnsidW5pZmllZF9hcGlzIjpbImVtYWlsIiwiY3VzdG9tZXItc3VwcG9ydCJdLCJoaWRlX3Jlc291cmNlX3NldHRpbmdzIjpmYWxzZSwic2FuZGJveF9tb2RlIjpmYWxzZSwiaXNvbGF0aW9uX21vZGUiOmZhbHNlLCJzZXNzaW9uX2xlbmd0aCI6IjFoIiwic2hvd19sb2dzIjp0cnVlLCJzaG93X3N1Z2dlc3Rpb25zIjpmYWxzZSwic2hvd19zaWRlYmFyIjp0cnVlLCJhdXRvX3JlZGlyZWN0IjpmYWxzZSwiaGlkZV9ndWlkZXMiOmZhbHNlLCJhbGxvd19hY3Rpb25zIjpbImRpc2FibGUiLCJkaXNhYmxlIl19LCJjb25zdW1lcl9pZCI6InRlc3QtY29uc3VtZXItbmV3IiwiYXBwbGljYXRpb25faWQiOiJQSk5NWEdiUG81M2kwN2tmSVZkMzBpbXVCVVEzSlJDbGNRZm5JVmQ0Iiwic2NvcGVzIjpbXSwiZGF0YV9zY29wZXMiOnsiZW5hYmxlZCI6ZmFsc2V9LCJpYXQiOjE3NTY5OTEzMTgsImV4cCI6MTc1Njk5NDkxOH0.mI4tFmyOgWuvQyiDJfEC-sSxvzFSWZTxYK6kx6KdZO0"
   />
 );
 
@@ -86,6 +86,18 @@ CusotmMappingView.args = {
   unifiedApi: 'accounting',
   serviceId: 'quickbooks',
   initialView: 'custom-mapping',
+};
+
+export const AutoStartAuthorization = Template.bind({});
+AutoStartAuthorization.args = {
+  trigger: (
+    <button className="p-2 border rounded shadow">
+      Open auto start authorization
+    </button>
+  ),
+  unifiedApi: 'accounting',
+  serviceId: 'quickbooks',
+  autoStartAuthorization: true,
 };
 
 export const SingleUnifiedApi = Template.bind({});
